ff7c9b10950f6680dddb852ebccad2c228180cd8
GoF9490 <dw9490@naver.com>
2022-04-02 21:42:29 +0900

    0402x

--- BTSR_git/Assets/Script/TextManager.cs
+++ BTSR_git/Assets/Script/TextManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //스트링 리스트를 담고있는 스크립트 하나를 만들고, 그 스크립트를 상속받는 스크립트에 원하는 텍스트를 입력,
-//이 스크립트에서 스트링 배열을 불러와서 해당 길이를 불러와 길이에 따른 스트링 리스트를 호출해 표현, 길이가 끝에 다다르면 종료하게끔.
+//이 스크립트에서 스트링 배열을 불러와서 해당 길이를 불러와 길이에 따른 스트링 리스트를 호출해 표현, 길이가 끝에 다다르면 종료하게끔.asdf
 public class TextManager : MonoBehaviour
 {
     
